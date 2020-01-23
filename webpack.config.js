42d4835e43270d126e60f2368e52f0113ce3b4d4
Слепнев Александр Юрьевич <ayslepnev@domclick.ru>
2020-01-23 09:23:02 +0300

    webpack: Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
+  context: path.resolve(__dirname, 'src'),
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    analytics: './src/analytics.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'public') + '/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ]
 }
